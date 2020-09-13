c4e7cf11feef0948d78e049866778449f3f3bf33
shiv-kirdat <shivalikirdat@gmail.com>
2020-09-13 21:00:18 +0530

    adding recycler

---
+++ app/src/main/java/com/example/adminside_ngo/eventrecycler.java
@@ -0,0 +1,14 @@
+package com.example.adminside_ngo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class eventrecycler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventrecycler);
+    }
+}
